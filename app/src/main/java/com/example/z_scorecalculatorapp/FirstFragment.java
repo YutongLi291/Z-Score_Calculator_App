e3893bac440df21d539e294e7a4da45c1bf9b1b4
Yutong Li <rainbowzom@gmail.com>
2019-12-22 22:40:19 -0800

    added spinner thing

--- app/src/main/java/com/example/z_scorecalculatorapp/FirstFragment.java
+++ app/src/main/java/com/example/z_scorecalculatorapp/FirstFragment.java
@@ -1,13 +1,14 @@
 package com.example.z_scorecalculatorapp;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -20,6 +21,7 @@ public class FirstFragment extends Fragment {
     EditText facultyEntered;
     EditText courseNumberEntered;
     GradesApi gradesApi;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -30,9 +32,9 @@ public class FirstFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Spinner facultyDropdown = view.findViewById(R.id.select_course)
 
 
         view.findViewById(R.id.calculate_button).setOnClickListener(new View.OnClickListener() {
