2cd488e48aecbd08a7742042d28d1b8419c1d78f
Yutong Li <rainbowzom@gmail.com>
2019-12-23 12:06:56 -0800

    need to fix zscore getting process bufferedreader

--- app/src/main/java/com/example/z_scorecalculatorapp/FirstFragment.java
+++ app/src/main/java/com/example/z_scorecalculatorapp/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.z_scorecalculatorapp;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,22 +8,29 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import network.GradesApi;
 
 public class FirstFragment extends Fragment {
 
 
-    EditText scoreEntered;
-    EditText facultyEntered;
-    EditText courseNumberEntered;
+    EditText scoreField;
+    EditText facultyField ;
+    EditText courseField;
+    GradesApi gradesApi;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-
+        View v =inflater.inflate(R.layout.fragment_first, container, false);
+         scoreField = v.findViewById(R.id.score_field);
+         facultyField = v.findViewById(R.id.faculty_field);
+        courseField = v.findViewById(R.id.courseNumber_field);
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
@@ -31,17 +39,24 @@ public class FirstFragment extends Fragment {
 
 
         view.findViewById(R.id.calculate_button).setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View view) {
-                 scoreEntered = view.findViewById(R.id.score_field);
-                Integer myScore = Integer.parseInt(scoreEntered.getText().toString());
-                 facultyEntered = view.findViewById(R.id.faculty_field);
-                String theFaculty = facultyEntered.getText().toString();
-                courseNumberEntered = view.findViewById(R.id.courseNumber_field);
-                Integer courseNumber = Integer.parseInt(courseNumberEntered.getText().toString());
+//                 scoreEntered = (EditText)view.findViewById(R.id.score_field);
+                String  myScore =(scoreField.getText().toString());
+//                 facultyEntered = (EditText)view.findViewById(R.id.faculty_field);
+                String theFaculty =  facultyField.getText().toString();
+//                courseNumberEntered =(EditText) view.findViewById(R.id.courseNumber_field);
+                String courseNumber = (courseField.getText().toString());
+                gradesApi = new GradesApi(theFaculty, courseNumber, myScore);
+
+
                 FirstFragmentDirections.ActionFirstFragmentToSecondFragment action =
-                        FirstFragmentDirections.
-                                actionFirstFragmentToSecondFragment("From FirstFragment");
+                        null;
+                double zscore = gradesApi.searchCourseThenGetZscore();
+                    action = FirstFragmentDirections.
+                            actionFirstFragmentToSecondFragment(zscore);
+
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(action);
             }
